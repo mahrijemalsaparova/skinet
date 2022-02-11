211a9eb47213a700ee6f40cd7910676166ba950f
unknown <mahrijemal.saparova@gmail.com>
2022-02-11 15:44:08 +0300

    initial create

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,9 @@
+namespace Core.Entities
+{
+    public class Product
+    {
+        //coloumns
+        public int Id { get; set; } //primary key
+        public string? Name { get; set; }
+    }
+}
